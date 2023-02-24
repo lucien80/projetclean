3ec05d6a88d6037740855ba241878fbfa5f8b08f
lucien80 <55386141+lucien80@users.noreply.github.com>
2023-02-24 05:21:09 +0100

    Update menudep33.py

--- menudep33.py
+++ menudep33.py
@@ -12,21 +12,28 @@ ______                      _                            _         _        _   
           |_|   
 """
 
-# Display the logo
-print(logo)
-
 # Options menu
-print("Choisissez une option :")
-print("1 - Exécuter le fichier 'script1.py'")
-print("2 - Exécuter le fichier 'script2.py'")
-
-# User input
-choice = input("> ")
+def show_menu():
+    print("\n" + logo)
+    print("Choisissez une option :")
+    print("1 - Exécuter le fichier 'script1.py'")
+    print("2 - Exécuter le fichier 'script2.py'")
 
 # Execute the selected script
-if choice == "1":
-    exec(open("script1.py").read())
-elif choice == "2":
-    exec(open("script2.py").read())
-else:
-    print("Option invalide.")
+def execute_script(choice):
+    if choice.lower() == "script1":
+        print("\nExécution de 'script1.py'...")
+        exec(open("script1.py").read())
+    elif choice.lower() == "script2":
+        print("\nExécution de 'script2.py'...")
+        exec(open("script2.py").read())
+    else:
+        print("\nOption invalide. Veuillez sélectionner une option valide.")
+
+# Main loop
+while True:
+    show_menu()
+    choice = input("\n> ").strip().lower()
+    if not choice:
+        continue
+    execute_script(choice)
