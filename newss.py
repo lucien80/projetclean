fbc41068ab226f1c41788cf24d7acfc7a0796332
lucien80 <55386141+lucien80@users.noreply.github.com>
2023-02-26 15:21:34 +0100

    Update newss.py

--- newss.py
+++ newss.py
@@ -6,42 +6,37 @@ import subprocess
 
 def analyze_usb():
     # Recherche de toutes les clés USB connectées
-    devices = glob.glob('/media/*')
+    devices = glob.glob('/dev/sd*')
+    if not devices:
+        print("Aucune clé USB n'est connectée.")
+        sys.exit()
+        
     print("Clés USB connectées :")
     for i, device in enumerate(devices):
-        print(f"{i+1}. {device} ({round(get_usb_size(device)/1024/1024/1024, 2)} GB)")
+        print(f"{i+1}. {device}")
     print("")
 
     # Sélection de la clé USB à analyser
     selected_device = int(input("Choisissez la clé USB à analyser (entrez le numéro) : ")) - 1
     selected_device = devices[selected_device]
 
-    # Demande si l'utilisateur souhaite monter la clé USB s'il n'est pas déjà monté
-    if not selected_device.startswith('/media/'):
-        mount_device = input(f"La clé USB {selected_device} n'est pas montée. Souhaitez-vous la monter automatiquement ? (o/n) : ")
-        if mount_device.lower() == 'o':
-            mount_point = '/media/' + selected_device.split('/')[-1]
-            os.makedirs(mount_point, exist_ok=True)
-            subprocess.run(['mount', selected_device, mount_point])
-            selected_device = mount_point
+    # Monter la clé USB si elle n'est pas déjà montée
+    mount_point = '/media/' + selected_device.split('/')[-1]
+    os.makedirs(mount_point, exist_ok=True)
+    subprocess.run(['mount', selected_device, mount_point])
+    selected_device = mount_point
 
     # Analyse de la clé USB avec ClamAV
     print(f"Analyse de la clé USB {selected_device} en cours...")
-    result = subprocess.run(['clamscan', '-r', selected_device], stdout=subprocess.PIPE)
-    result = result.stdout.decode('utf-8')
-
-    # Affichage des résultats de l'analyse
-    print(result)
-
-def get_usb_size(path):
-    total = 0
-    with os.scandir(path) as it:
-        for entry in it:
-            if entry.is_file():
-                total += entry.stat().st_size
-            elif entry.is_dir():
-                total += get_usb_size(entry.path)
-    return total
+    process = subprocess.Popen(['clamscan', '-r', selected_device], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    while True:
+        output = process.stdout.readline().decode('utf-8')
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            print(output.strip())
+        time.sleep(0.1)
+    print("Analyse terminée.")
 
 if __name__ == '__main__':
     analyze_usb()
